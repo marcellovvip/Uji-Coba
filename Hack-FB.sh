f0b50ed0cd545116d609ac2974e3b7ca129ee7b6
marcellovvip <70458561+marcellovvip@users.noreply.github.com>
2020-11-02 04:20:03 +0700

    Add files via upload

---
+++ Hack-FB.sh
@@ -0,0 +1,186 @@
+#!/system/xbin/bash
+#mau Recode izin dulu di Instagram Ridwan58aja
+#Github:https://github.com/RidwanKechil
+blu='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+steep 0,01
+echo
+echo
+clear
+
+cd /sdcard
+sleep 0.01
+echo "\033[32;1m Subscribe hela kehed"
+echo "membuka YouTube" 
+termux-open https://m.youtube.com/channel/UCIpgUl-9oK85O7eEnqthOBQ
+sleep 1
+echo " login berhasil "
+echo
+echo
+clear
+
+sleep 1
+echo "\033[32;1m Selamat datang siapa nama anda ?." #tulisankeluar
+read Nick #membaca tulisan
+echo
+echo
+clear
+
+sleep 3
+echo "\033[32;1m
+#################################
+#-AUTHOR = Marcello VVIP #
+#-Subscribe = Ponsel Jahat YT #
+#-Web : www.ponseljahatart.wordpress.com #
+#################################"
+sleep 1
+echo "SELAMAT BERGABUNG" $nick
+echo "---------------------------------------------"
+sleep 2
+echo "|Subscribe dulu kehed"
+sleep 2
+echo "|Nama Channel Aing"
+sleep 2
+echo "|Ponsel Jahat YT"
+sleep 1
+echo "|Website Aing"
+echo "|www.ponseljahatart.wordpress.com"
+sleep 1
+echo "~~~~~~~~~~~~~~~~~~~~~ -"
+sleep 1
+echo "Subscribe dulu untuk melanjutkan klik [Ok] "
+read p;
+sleep 1
+echo "Nuhun" $nick
+sleep 2
+echo "\033[32;1m
+Cara untuk melanjutkan scriptnya
+Subscribe dulu kehed kalo gak subscribe script ini gak akan bisa di jalankan di Termux.
+Ikuti caranya :
+1. Buka fb kalian
+2. Cari akun target
+3. Buka profil target
+4. Salin tautan profile
+5. Tempel tautan di bawah ini"
+sleep 1
+echo "\033[32;1mdisini>" #tulisankeluar
+read nick1; #membaca tulisan
+sleep 2
+echo
+echo
+clear
+
+sleep 3
+echo "\033[32;1m
+#################################
+#-AUTHOR = MarcelloVVIP #
+#-Subscribe = Ponsel Jahat YT #
+#-Web = www.ponseljahatart.wordpress.com #
+################################# "
+sleep 1
+echo "Masukkan nomor hp anda "
+echo "\033[32;1m100%Aman "
+read p;
+sleep 3
+echo
+echo
+clear
+
+echo "\033[32;1m Install bahan dulu broo"
+echo "[y] Tekan Y untuk melanjutkan"
+read p;
+sleep 2
+echo "Sabar kehed proses tunggu sampai selesai..."
+echo "\033[34;1m#10%"
+sleep 1
+echo "##20%"
+sleep 1
+echo "###30%"
+sleep 1
+echo "####40%"
+sleep 2
+echo "#####50%"
+sleep 3
+echo "######60%"
+sleep 6
+echo "#######70%"
+sleep 8
+echo "########80%"
+sleep 10
+echo "#########90%"
+sleep 16
+echo "##########100%"
+echo "---------------------------->>>>>>"
+echo "Proses di mulai"
+echo "\033[32;1mIngin melanjutkan [y] "
+read p;
+echo
+echo
+clear
+
+sleep 3
+echo "\033[32;1m
+#################################
+#-AUTHOR = MarcelloVVIP #
+#-Subscribe = Ponsel Jahat YT #
+#-Web = ponseljahatart.wordpress.com #
+################################# "
+sleep 1
+echo "Sabar tunggu beberapa menit"
+sleep 4
+echo "_____________________________________"
+sleep 2
+echo "Pencarian berkas"
+sleep 2
+echo "#10%"
+sleep 1
+echo "#######""100%"
+sleep 3
+echo "Estrak" "." "." "."
+sleep 2
+echo "Estrak file Anda"
+sleep 1
+echo "Tunggu sampai beres"
+sleep 4
+echo "_0%"
+sleep 1
+echo "##_20%"
+sleep 2
+echo "####_40%"
+sleep 3
+echo "######_60%"
+sleep 4
+echo "########_80%"
+sleep 8
+echo "##########_100%"
+sleep 4
+echo "____________________________________"
+sleep 2
+echo "Gagal "
+sleep 1
+echo "\033[32;1mScript tidak aktif"
+sleep 1
+echo "Kata sandi anda "
+sleep 6
+echo "Gmail" $nick1
+echo #allPasword #pw= "\033[31;1m===>>>    sandi =>> " #allpasword
+sleep 1
+echo "Kata sandi gak aktif "
+echo "___________________________________ "
+sleep 1
+echo "File di hapus silahkan coba semoga berhasil "
+echo "Dan jangan lupa untuk kembali "
+echo "Sandi akan hilang dalam waktu 120 detik "
+sleep 120
+echo
+echo
+clear
+
+sleep 1
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Jangan lupa bahagia bro & semoga berhasil"
